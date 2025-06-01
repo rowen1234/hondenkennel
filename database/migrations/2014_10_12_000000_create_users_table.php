8a1de5126e024b8682c3cf1bebfa810f099d3644
Rowen Bieshaar <rowen.04@hotmail.com>
2025-06-01 20:05:33 +0200

    install breeze

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->foreignId('dog_id');
+            $table->boolean('admin');
             $table->rememberToken();
             $table->timestamps();
         });
