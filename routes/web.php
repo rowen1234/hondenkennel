9d6b905a3fda9e2e1c31cb3770e214dd61bd455a
Rowen Bieshaar <rowen.04@hotmail.com>
2025-06-01 22:32:14 +0200

    move route

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('dog', DogController::class);
 });
-Route::resource('dog', DogController::class);
 
 require __DIR__.'/auth.php';
